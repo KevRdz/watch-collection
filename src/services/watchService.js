e24e14f7a098cd4153b2a608df59699ed3e849fd
Kevin <kevin.rodriguez678@gmail.com>
2024-01-30 13:21:01 -0500

    create delete function in watchservice to delete watch in the back end

--- src/services/watchService.js
+++ src/services/watchService.js
@@ -3,19 +3,15 @@ import * as tokenService from './tokenService'
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/watches`
 
 async function create(watch) {
-  try {
-    const res = await fetch(BASE_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${tokenService.getToken()}`
-      },
-      body: JSON.stringify(watch)
-    })
+  const res = await fetch(BASE_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${tokenService.getToken()}`
+    },
+    body: JSON.stringify(watch)
+  })
     return res.json()
-  } catch (err) {
-    console.log(err);
-  }
 }
 
 async function getAll() {
@@ -23,7 +19,18 @@ async function getAll() {
   return res.json()
 }
 
+async function deleteOne(id) {
+  const res = await fetch(`${BASE_URL}/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${tokenService.getToken()}`
+    }
+  })
+  return res.json()
+}
+
 export {
   create,
   getAll,
+  deleteOne,
 }
